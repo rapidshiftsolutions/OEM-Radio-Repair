f76c42cc5c374f945e4ca5019e828e5bbfc70ff6
OEMRadioRepair <oemradiorepair@gmail.com>
2023-11-05 21:51:40 -0600

    h

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,6 @@ import Contact from '@/components/Contact'
 import  PrimaryFeatures  from '@/components/AboutUs'
 import SecondaryFeatures from '@/components/VehicleMakes'
 import { Testimonials } from '@/components/Testimonials'
-import  Banner  from '@/components/Banner'
 
 import { Route, Routes } from 'react-router-dom'
 

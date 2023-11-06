f520688ef467740815fc1ecd109173dfb0107092
OEMRadioRepair <oemradiorepair@gmail.com>
2023-11-05 22:18:31 -0600

    z

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ export default function Example() {
   return (
     <>
       <div id="contact" className="m-auto flex justify-center py-6 sm:py-12 bg-blue-200">
-        <div className="max-w-4/5 w-auto">
+        <div id="cont" className="max-w-4/5 w-auto">
           <iframe className="w-screen"
             src="https://docs.google.com/forms/d/e/1FAIpQLSc9ZmFaK7210vgtQ87_wnylrURXRMXwA3EGHNzJU2T_cSJlTg/viewform?embedded=true"
             frameborder="0"

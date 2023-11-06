59afe39fbb0a81a8173f56d133539030c99e51c3
OEMRadioRepair <149844161+OEMRadioRepair@users.noreply.github.com>
2023-11-06 14:14:11 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,16 +43,7 @@ export function Hero() {
       >
         Learn More
       </button></a>
-          <svg
-            aria-hidden="true"
-            className="h-3 w-3 flex-none fill-blue-600 group-active:fill-current"
-          >
-            <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
-          </svg>
-          <span className="ml-3">Learn More</span>
-        </Button>
       </div>
-      
     </Container>
   )
 }

4adf49fafeffdf585fc90e896bdae6638ff6534c
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2023-11-07 11:34:23 -0600

    Lazy Contact

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,33 @@
-import  ForDealerships  from '@/components/ForDealerships'
-import { Faqs } from '@/components/Faqs'
-import { Footer } from '@/components/Footer'
-import  Header  from '@/components/Header'
+import React, {Suspense} from 'react';
+
+import ForDealerships from '@/components/ForDealerships'
+import {Faqs} from '@/components/Faqs'
+import {Footer} from '@/components/Footer'
+import Header from '@/components/Header'
 import {Hero} from '@/components/Hero'
 import Contact from '@/components/Contact'
-import  PrimaryFeatures  from '@/components/AboutUs'
+import PrimaryFeatures from '@/components/AboutUs'
 import SecondaryFeatures from '@/components/VehicleMakes'
-import { Testimonials } from '@/components/Testimonials'
+import {Testimonials} from '@/components/Testimonials'
+
+const LazyContact = React.lazy(() => import ('../components/Contact'))
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-        <PrimaryFeatures />
-        <SecondaryFeatures />
-        <ForDealerships />
-        <Contact />
-      </main>
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <main>
+                <Hero/>
+                <PrimaryFeatures/>
+                <SecondaryFeatures/>
+                <ForDealerships/>
+                <Suspense fallback={
+                    <div>Loading...</div>
+                }>
+                    <LazyContact/>
+                </Suspense>
+            </main>
+            <Footer/>
+        </>
+    )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,7 @@ export default function Example() {
   return (
     <>
       <div id="contact" className="m-auto flex justify-center py-6 sm:py-12 bg-blue-200">
+        
         <div id="cont" className="max-w-4/5 w-auto">
           <iframe className="w-screen"
             src="https://docs.google.com/forms/d/e/1FAIpQLSc9ZmFaK7210vgtQ87_wnylrURXRMXwA3EGHNzJU2T_cSJlTg/viewform?embedded=true"

aa39f90185c6cddfc26a9e0bb04a6c6512052956
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-11-18 12:01:44 -0600

    a

--- src/components/nj_footer.jsx
+++ src/components/nj_footer.jsx
@@ -1,19 +1,21 @@
+'use client'
+
+import Image from 'next/image'
+
 const navigation = {
-  contact: [
-    { name: 'Contact Us', href: '/contact' },
-    { name: 'Get a Quote', href: '/get-a-quote' },
+  information: [
+    { name: 'About Us', href: '/about' },
+    { name: 'Locations', href: '/locations' },
     { name: 'Careers', href: '/careers' },
   ],
   services: [
-    { name: 'Repair Services', href: '/services/repair' },
     { name: 'Amplifier Repair', href: '/services/amplifier-repair' },
     { name: 'Factory Touch Screen Repair', href: '/services/touch-screen-repair' },
-    { name: 'Warranty Services', href: '/services/warranty' },
   ],
-  information: [
+  legal: [
     { name: 'Terms of Service', href: '/terms' },
     { name: 'Privacy Policy', href: '/privacy-policy' },
-    { name: 'FAQ', href: '/faq' },
+    { name: 'Warranty', href: '/warranty' },
   ],
 };
 
@@ -23,18 +25,20 @@ export default function Footer() {
       <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8 lg:py-32">
         <div className="xl:grid xl:grid-cols-4 xl:gap-8">
           <div className="flex items-center justify-center xl:col-span-1 max-w-screen-sm">
-            <img
+            <Image
               alt="OEM Radio Repair Logo"
               src="/OEMRadioRepair/OEMRadioRepair-w.svg" // Update with the actual logo path
-              className="h-8 pr-0 md:pr-24"
+              width={200}
+              height={80}
+              className="w-2/3 h-auto" // Ensures the aspect ratio is maintained
             />
           </div>
           <div className="mt-16 grid grid-cols-1 gap-8 xl:col-span-3 xl:mt-0">
             <div className="grid grid-cols-2 gap-8 md:grid-cols-3">
               <div>
-                <h3 className="text-lg font-semibold text-light-50">Contact</h3>
+                <h3 className="text-lg font-semibold text-light-50">Information</h3>
                 <ul role="list" className="mt-6 space-y-4">
-                  {navigation.contact.map((item) => (
+                  {navigation.information.map((item) => (
                     <li key={item.name}>
                       <a href={item.href} className="text-sm text-gray-300 hover:text-light-50">
                         {item.name}
@@ -56,9 +60,9 @@ export default function Footer() {
                 </ul>
               </div>
               <div>
-                <h3 className="text-lg font-semibold text-light-50">Information</h3>
+                <h3 className="text-lg font-semibold text-light-50">Legal</h3>
                 <ul role="list" className="mt-6 space-y-4">
-                  {navigation.information.map((item) => (
+                  {navigation.legal.map((item) => (
                     <li key={item.name}>
                       <a href={item.href} className="text-sm text-gray-300 hover:text-light-50">
                         {item.name}

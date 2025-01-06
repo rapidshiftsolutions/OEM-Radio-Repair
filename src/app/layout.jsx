da12f859a5f9a88d3e77b23f59b42fcef68061fb
rapidshiftsolutions <nathan@jonesco.autos>
2025-01-06 13:08:18 -0600

    a

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <script src="https://analytics.ahrefs.com/analytics.js" data-key="tgAseXOCr25EjtPoO0MNSw" async></script>
         <link
           rel="stylesheet"
           href="https://api.fontshare.com/css?f%5B%5D=switzer@400,500,600,700&amp;display=swap"
@@ -21,7 +22,7 @@ export default function RootLayout({ children }) {
           title="The OEM Radio Repair  Blog"
           href="/blog/feed.xml"
         />
-        <script src="https://analytics.ahrefs.com/analytics.js" data-key="tgAseXOCr25EjtPoO0MNSw" async></script>
+        
       </head>
       <body className="text-gray-950 antialiased">{children}</body>
     </html>
